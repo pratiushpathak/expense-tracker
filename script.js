4b2f412daea738a75be3c241e72d4afe6229e4f4
pratiush pathak <pratiushpathak@gmail.com>
2025-08-14 20:34:15 +0530

    display balance income and expense

--- script.js
+++ script.js
@@ -28,9 +28,34 @@ ${transaction.text} <span>${sign} ${Math.abs(transaction.amount)} </span><button
 `
 list.appendChild(item);
 }
+
+//update the balance income and expense
+function updateValues()
+{
+  const amounts=transaction.map((transaction)=>{
+       return  transaction.amount;
+  });
+  const total=amounts.reduce((acc,item)=>(acc+=item),0).toFixed(2);
+  // console.log(total);
+  const income=amounts.filter(item=>item>0)
+               .reduce((acc,item)=>(acc+=item),0)
+               .toFixed(2);
+
+    // console.log(income);
+    const expense=(amounts.filter(item=>item<0)
+                  .reduce((acc,item)=>(acc+=item),0)*-1)
+                  .toFixed(2);
+    // console.log(expense)
+    balance.innerText=`$${total}`;
+    money_plus.innerText=`$${income}`;
+    money_minus.innerText=`$${expense}`;
+
+}
 function init()
 {
   list.innerHTML='';
   transaction.forEach(addTransactionDOM)
+  updateValues();
 }
+
 init();
